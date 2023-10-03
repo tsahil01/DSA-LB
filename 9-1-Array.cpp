036d99c3c88b5a53b0650cfaede5fb18f0d07e5c
Sahil Tiwaskar <118750218+tsahil01@users.noreply.github.com>
2023-10-03 17:28:49 +0000

    Array-1

---
+++ 9-1-Array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+void PrintArray(int arr[], int s ){ // no need to give size of array here
+    // int s = sizeof(arr)/sizeof(int);
+    for(int i = 0; i<s; i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+}
+
+int main(){
+    int a[5] = {10,20};  // Automatically all the remaining elements will be 0. i.e.--> 10, 20, 0, 0, 0
+    int s = sizeof(a)/sizeof(int); // To get size of array
+    PrintArray(a,s);
+    return 0;
+}
+
